7549598dd68485ea57073cb3bc0fd7847216f1ee
HikaruShiho <shiho97797@gmail.com>
2022-09-16 20:21:31 +0900

    .env追加

--- firebase/config.tsx
+++ firebase/config.tsx
@@ -3,16 +3,17 @@
  * 【参考】
  * https://stackoverflow.com/questions/72179070/react-native-bundling-failure-error-message-while-trying-to-resolve-module-i
  */
+import { API_KEY, AUTH_DMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from 'react-native-dotenv';
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDAP1JLkU5Cj24V6P1ohQQJBEIbAV2rdUc",
-    authDomain: "react-native-auth-8cfc3.firebaseapp.com",
-    projectId: "react-native-auth-8cfc3",
-    storageBucket: "react-native-auth-8cfc3.appspot.com",
-    messagingSenderId: "277084128067",
-    appId: "1:277084128067:web:60c00a94015514819f25f6"
+    apiKey: API_KEY,
+    authDomain: AUTH_DMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
